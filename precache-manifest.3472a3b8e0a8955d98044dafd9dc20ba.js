05260aaddf3a2332f2e85cdb28892f0e6b4f100f
zayac <zaicevvv1991@gmail.com>
2019-05-04 19:53:41 +0300

    Updates

---
+++ precache-manifest.3472a3b8e0a8955d98044dafd9dc20ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b171fb2fb127ea450844f39b08fc986c",
+    "url": "/goit-react-hw-01/index.html"
+  },
+  {
+    "revision": "1554ba9a89d10cda8ebd",
+    "url": "/goit-react-hw-01/static/css/main.6c2fd030.chunk.css"
+  },
+  {
+    "revision": "30d772f406668a7544c4",
+    "url": "/goit-react-hw-01/static/js/2.6296308c.chunk.js"
+  },
+  {
+    "revision": "1554ba9a89d10cda8ebd",
+    "url": "/goit-react-hw-01/static/js/main.46112413.chunk.js"
+  },
+  {
+    "revision": "f51632aec18d5636ad74",
+    "url": "/goit-react-hw-01/static/js/runtime~main.bdf556df.js"
+  }
+]);
