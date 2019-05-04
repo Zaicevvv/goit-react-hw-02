2c0bee26cda60242982c86025b80036779cb6335
zayac <zaicevvv1991@gmail.com>
2019-05-04 13:56:28 +0300

    test

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class App extends Component {
+  state = {};
+  render() {
+    return <div>qwe</div>;
+  }
+}
